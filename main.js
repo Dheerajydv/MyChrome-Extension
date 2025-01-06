c0209fa0df3d703c5986619edb7e415b5f24c00b
Dheerajydv <dy81035@gmail.com>
2025-01-06 17:04:14 +0530

    Done

---
+++ main.js
@@ -0,0 +1,7 @@
+const apiLink =
+  "https://api.quotable.io/quotes/random?tags=technology,famous-quotes";
+
+const quoteText = document.getElementById("quote");
+quoteText.addEventListener("click", () => {
+  console.log("Clicked");
+});
